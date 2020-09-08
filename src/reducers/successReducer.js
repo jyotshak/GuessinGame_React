8cd68909a39e48f6e79170efab371e8ca1b231ba
Jyotsha Kumar <legiondrake@gmail.com>
2020-09-09 00:38:21 +0530

    progress in actions

--- src/reducers/successReducer.js
+++ src/reducers/successReducer.js
@@ -1,3 +1,11 @@
-export default (state,action) => {
-    return null;
+import { act } from "react-dom/test-utils";
+import { actionTypes } from "../actions";
+
+export default (state=false,action) => {
+    switch(action.type) {
+        case(actionTypes.CORRECT_GUESS):
+            return true;
+        default:
+            return state;
+    }
 }
