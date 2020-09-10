cd896276cc3d7869aac4fae981e734f0d6524826
Jyotsha Kumar <legiondrake@gmail.com>
2020-09-10 14:38:44 +0530

    Moxios for axios calls

--- src/test/testUtils.js
+++ src/test/testUtils.js
@@ -8,12 +8,7 @@ export const storeFactory= (initialState) =>{
   const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore);
   return createStoreWithMiddleware(rootReducer, initialState);
 }
-/**
- * Return node(s) with the given data-test attribute.
- * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper.
- * @param {string} val - Value of data-test attribute for search.
- * @returns {ShallowWrapper}
- */
+
 export const findByTestAttr = (wrapper, val) => {
   return wrapper.find(`[data-test="${val}"]`);
 }
